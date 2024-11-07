ed225029443526c4f99cc23ff563b3fc94298e29
daichi0825 <daichi_0825_0324@yahoo.co.jp>
2024-11-07 13:58:57 +0900

    test

--- testGit/src/test/GitTest.java
+++ testGit/src/test/GitTest.java
@@ -2,8 +2,9 @@ package test;
 
 public class GitTest {
 	public static void main(String[] args) {
-		System.out.println("Hello GitHub!");
 		System.out.println("Goodmorning GitHub!");
+		System.out.println("Hello GitHub!");
+		System.out.println("Goodbye GitHub!");
 	}
 
 }
